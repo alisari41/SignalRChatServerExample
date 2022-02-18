3df52809bd816f90180e9a76374243b681d9a78b
alisari41 <alisari41@outlook.com>
2022-02-18 20:22:27 +0300

    Server'a bağlanan client'ları listeleme işlemleri

---
+++ SignalRChatServerExample/Data/ClientSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SignalRChatServerExample.Models;
+
+namespace SignalRChatServerExample.Data
+{
+    public static class ClientSource
+    {
+        public static List<Client> Clients { get; } = new List<Client>();
+
+    }
+}
--- SignalRChatServerExample/Hubs/ChatHub.cs
+++ SignalRChatServerExample/Hubs/ChatHub.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using SignalRChatServerExample.Data;
+using SignalRChatServerExample.Models;
 
 namespace SignalRChatServerExample.Hubs
 {
     public class ChatHub : Hub
     {
-
+        public async Task GetNickName(string nickName)
+        {
+            Client client = new Client
+            {
+                ConnectionId = Context.ConnectionId,
+                NickName = nickName
+            };
+            ClientSource.Clients.Add(client);//Clientlarımı depoluyorum
+            await Clients.Others.SendAsync("clientJoined", nickName);
+            await Clients.All.SendAsync("clients", ClientSource.Clients);//Giriş yapan clientları bir listede tutmak için
+        }
     }
 }
